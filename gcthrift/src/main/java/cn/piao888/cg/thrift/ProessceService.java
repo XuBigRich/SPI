42871247f9e65b0656ecd26fd6623a5c3b7aaced
mac <847118663@qq.com>
2020-06-02 19:18:55 +0800

    提交

--- gcthrift/src/main/java/cn/piao888/cg/thrift/ProessceService.java
+++ gcthrift/src/main/java/cn/piao888/cg/thrift/ProessceService.java
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-package cn.piao888.cg.thrift;
-
-import cn.piao888.ProcessorDTO;
-import cn.piao888.Proctocol;
-import cn.piao888.cg.DTO.OrgService;
-import cn.piao888.cg.service.OrgDemo;
-
-public class ProessceService implements Proctocol {
-
-    @Override
-    public ProcessorDTO registerProcessor() {
-        ProcessorDTO processorDTO=new ProcessorDTO();
-        processorDTO.setProcessorName("org");
-        processorDTO.setProcessor(new OrgService.Processor(new OrgDemo()));
-        System.out.println("hello world!");
-        return processorDTO;
-    }
-}
-=======
 package cn.piao888.cg.thrift;
 
 import cn.piao888.ProcessorDTO;
@@ -47,4 +27,3 @@ public class ProessceService implements Proctocol {
         return processorDTOS;
     }
 }
->>>>>>> d9fed244bc331bfdb9fbdb83eefa515d68770e37
